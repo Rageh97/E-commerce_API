e0f5bb9480e2e81d056d206d40daddc336809c99
Rageh97 <abdelhady412rageh@gmail.com>
2023-09-14 18:29:30 +0300

    complete crud operations of categories

--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -4,9 +4,11 @@ const {
   getCategory,
   createCategory,
   getSpecificCategory,
+  updateCategory,
+  deleteCategory
 } = require("../Controllers/categoryController");
 
 router.route("/").get(getCategory).post(createCategory);
-router.route("/:id").get(getSpecificCategory);
+router.route("/:id").get(getSpecificCategory).put(updateCategory).delete(deleteCategory);
 
 module.exports = router;

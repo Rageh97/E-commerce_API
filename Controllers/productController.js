428f5f7fede14198d91addc35aeec2cb54f87d26
Rageh97 <abdelhady412rageh@gmail.com>
2023-09-20 18:14:17 +0300

    Add sorting and searching functionalty

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -2,6 +2,7 @@ const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 const productModel = require("../Models/productModel");
 const ApiError = require("../Utils/apiError");
+
 // @desc create product
 // @route POST api/v1/products
 // @access private
@@ -14,14 +15,51 @@ exports.createProduct = asyncHandler(async (req, res) => {
 // @route GET api/v1/products
 // @access public
 exports.getProducts = asyncHandler(async (req, res) => {
+  // 1) Filtering
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const queryStringObj = { ...req.query };
+  const excludedFields = ["page", "fields", "limit", "sort"];
+  excludedFields.forEach((field) => delete queryStringObj[field]);
+  let queryStr = JSON.stringify(queryStringObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+  //
+  // 2) Pagination
+
   const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 5;
+  const limit = req.query.limit * 1 || 30;
   const skip = (page - 1) * limit;
-  const products = await productModel
-    .find()
+
+  // Building query
+  let mongooseQuery = productModel
+    .find(JSON.parse(queryStr))
     .skip(skip)
     .limit(limit)
     .populate({ path: "category", select: "name" });
+  // 3) Sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    mongooseQuery = mongooseQuery.sort(sortBy);
+  } else {
+    mongooseQuery = mongooseQuery.sort("-createAt");
+  }
+  // 4) Fields limiting
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    mongooseQuery = mongooseQuery.select(fields);
+  } else {
+    mongooseQuery = mongooseQuery.select("-_v");
+  }
+  //  5) searching
+  if (req.query.keyword) {
+    const query = {};
+    query.$or = [
+      { title: { $regex: req.query.keyword, $options: "i" } },
+      { description: { $regex: req.query.keyword, $options: "i" } },
+    ];
+    mongooseQuery = mongooseQuery.find(query);
+  }
+  // exccute query
+  const products = await mongooseQuery;
   res.status(200).json({ results: products.length, data: products });
 });
 // @desc get specific product
